9a294931652964da19777726d23ffc311ecfc943
Nadia Yudina <joystate@me.com>
2014-03-19 15:45:33 -0400

    Initial setup

---
+++ FlickrWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlickrWorld
+//
+//  Created by Nadia Yudina on 3/19/14.
+//  Copyright (c) 2014 Nadia Yudina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FlickrWorldTests/FlickrWorldTests.m
@@ -0,0 +1,34 @@
+//
+//  FlickrWorldTests.m
+//  FlickrWorldTests
+//
+//  Created by Nadia Yudina on 3/19/14.
+//  Copyright (c) 2014 Nadia Yudina. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FlickrWorldTests : XCTestCase
+
+@end
+
+@implementation FlickrWorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
