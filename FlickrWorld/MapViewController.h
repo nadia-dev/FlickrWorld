cd1646671a8740589db22babfab41a73b08cff89
Nadia Yudina <joystate@me.com>
2014-03-21 15:05:41 -0400

    Pin locations on map

--- FlickrWorld/MapViewController.h
+++ FlickrWorld/MapViewController.h
@@ -9,6 +9,6 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
-@interface MapViewController : UIViewController
+@interface MapViewController : UIViewController <MKMapViewDelegate>
 
 @end
--- FlickrWorld/MapViewController.m
+++ FlickrWorld/MapViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "MapViewController.h"
+#import "FlickrDataStore.h"
+#import "Place+Methods.h"
 
 @interface MapViewController ()
 
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) FlickrDataStore *dataStore;
 
 @end
 
@@ -20,7 +23,48 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
+    self.dataStore = [FlickrDataStore sharedDataStore];
+	self.mapView.delegate = self;
+    self.mapView.mapType = MKMapTypeHybrid;
+    
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:@"Place"];
+    NSArray *places = [self.dataStore.managedObjectContext executeFetchRequest:fetchRequest error:nil];
+    
+    
+    MKCoordinateRegion region = MKCoordinateRegionMake(self.mapView.centerCoordinate, MKCoordinateSpanMake(180, 360));
+    [self.mapView setRegion:region animated:YES];
+    
+    for (Place *place in places) {
+        
+        MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+        CLLocationCoordinate2D myCoordinate;
+        myCoordinate.latitude = [place.latutide floatValue];
+        myCoordinate.longitude = [place.longitude floatValue];
+        annotation.coordinate = myCoordinate;
+        [self.mapView addAnnotation:annotation];
+    }
+    
+}
+
+
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    static NSString *identifier = @"MyLocation";
+        
+    MKAnnotationView *annotationView = (MKAnnotationView *) [self.mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+    if (annotationView == nil) {
+        annotationView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier];
+        annotationView.enabled = YES;
+        annotationView.canShowCallout = YES;
+        annotationView.image = [UIImage imageNamed:@"arrest.png"];//here we use a nice image instead of the default pins
+    } else {
+        annotationView.annotation = annotation;
+    }
+    
+    return annotationView;
+
 }
 
 
