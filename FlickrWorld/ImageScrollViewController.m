2bc6cc59aeb8781a4f9c74a5cc21b5f51f4f8145
Nadia Yudina <joystate@me.com>
2014-04-24 18:04:44 -0400

    style change

--- FlickrWorld/ImageScrollViewController.m
+++ FlickrWorld/ImageScrollViewController.m
@@ -1,10 +1,3 @@
-//
-//  ImageScrollViewController.m
-//  ImageScroll
-//
-//  Created by Evgenii Neumerzhitckii on 19/05/13.
-//  Copyright (c) 2013 Evgenii Neumerzhitckii. All rights reserved.
-//
 
 #import "ImageScrollViewController.h"
 #import "UIImageView+AFNetworking.h"
